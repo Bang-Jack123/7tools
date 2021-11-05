8ef6a2ac73e3cda9fee6ff1a1b175fb9d6d71032
Bang-Jack123 <93056023+Bang-Jack123@users.noreply.github.com>
2021-11-05 17:15:13 +0700

    README.md
    install on termux
    $ pkg install Bash
    $ pkg install git
    $ git clone https://github.com/Bang-Jack123/7tools
    $ cd 7tools
    $ ls
    $ sh tools.sh

---
+++ tools.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/sh
+clear
+# code warna
+blue='\033[34;1m'	
+green='\033[32;1m'		
+purple='\033[35;1m'			
+cyan='\033[36;1m'				
+red='\033[31;1m'					
+white='\033[37;1m'						
+yellow='\033[33;1m'							
+read -p "Siapa Nama Kaka: " name;
+clear
+sleep 1
+echo $red
+figlet hay $name | lolcat
+sleep 3
+clear
+	echo $cyan "><><><><><><><><><><><><><><><>"
+	echo $blue "{√} welocome tu tools mr patrix "
+	echo $blue "{√} jangan iri jangan dengki [!] "
+	echo $red  "[!] Rekode boleh asal ijin dulu "
+	echo $blue "{?}  wa me : 081575706018 "
+	echo $cyan "><><><><><><><><><><><><><><><>"
+	echo
+	sleep 2
+	echo $cyan "><><><><><><><><><><><><><><><><>"
+	echo $blue "1"$bule")"$blue" TOOLS PENAMBAH KONTROL CONTOH : CTRL DLL" 
+	echo $blue "2"$blue")"$blue" TOOLS SPAM - WA "
+	echo $blue "3"$blue")"$blue" TOOLS CRACK FB BY MR PATRIX"
+	echo $blue "4"$blue")"$blue" TOOLS MR VIRUS [!] VIRUS "
+	echo $blue "5"$blue")"$blue" SUSBCIRBE YT SAYA YA :) "
+	echo $white "6"$white")"$white" SUSCRIBE YT KE 2 "
+	echo $blue "7"$blue")"$blue" keluar dan join grup "
+	echo $cyan "><><><><><><><><><><><><><><><><>"
+
+
+	sleep 1
+	read -p " Masukan pilihan anda : " pill
+	if [ $pill = 1 ]; then
+	sleep 1
+	echo
+	figlet loading | lolcat
+	git clone https://github.com/Bang-Jack123/termux
+	echo
+	clear		
+	sleep 1			
+	cd termux			
+	sleep 1					
+	python2 termux.py
+fi
+	if [ $pill = 2 ];then
+	sleep 1
+	echo
+	figlet loading ya | lolcat
+	git clone https://github.com/Bang-Jack123/Spam-wa
+	echo
+	clear
+	sleep 2		
+	cd Spam-wa		
+	sleep 3				
+	python2 WA.py				
+fi
+	if [ $pill = 3 ];then
+	echo
+	sleep 2
+	figlet sabar ya | lolcat
+	git clone https://github.com/Bang-Jack123/dihi
+	echo
+	clear
+	sleep 2
+	cd dihi
+	python2 dihi.py
+fi
+	    if [ $pill = 4 ];then
+	    echo
+	    figlet loading | lolcat
+    	    git clone https://github.com/mrvirusrecord/link-virus
+	    echo
+	    clear
+	    sleep 2
+	    cd link-virus
+	    sleep 3
+	    sh download.sh
+fi
+	    if [ $pill = 5 ];then
+	    figlet makasih | lolcat
+	    xdg-open https://youtube.com/channel/UCOqxx2kjYPypVct2l81Y1Jw
+	    sleep 2
+	    sh tools.sh
+fi
+
+
+	if [ $pill = 6 ];then
+	figlet YT BINTNG | lolcat
+	xdg-open https://youtube.com/channel/UCfq0IR6iJ2NuG0MW8hGif1w
+	sleep 2
+	sh tools.sh
+fi
+
+
+	    if [ $pill = 7 ];then
+	    echo $cyan "TERIMA KASIH TELAH MENGGUNAKAN TOOLS SAYA"
+	    echo $cyan "SALAM MR PATRIK [ ! ] "
+            read -p "[?] apakah kamu puas ? : " jawab
+	    echo  [√]makasih atas ulusan anda : $jawab
+	    sleep 2
+	    figlet open grup | lolcat
+	    xdg-open https://chat.whatsapp.com/K7uNXDBZTqxGG10EeBi1SG
+fi
+exit
